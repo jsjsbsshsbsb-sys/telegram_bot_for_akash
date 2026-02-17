7a90745ffb57db78afe27b9ea59600d694852837
F12 <jsjsbsshsbsb@gmail.com>
2026-02-17 16:23:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-TOKEN = "8564117995:AAEWiVbO7dx1PuMSGnhRt2rj7snn6tRas0g"
+TOKEN = "8564117995:AAE7qeTwhmN8y2WVg02t53zNNwIwvT1J5Cw"
 bot = telebot.TeleBot(TOKEN)
 
 # ===== Проверка подписки =====
